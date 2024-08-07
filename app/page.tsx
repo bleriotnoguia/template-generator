c8195d27e32cd8bd8209eccd687d9d629b859dcc
Bleriot Noguia <nstevebleriot@yahoo.fr>
2024-08-07 09:40:25 +0100

    chore: Update page template to use dynamic import for CustomEmailEditor component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
 "use client";
 
-import CustomEmailEditor from "@/components/CustomEmailEditor";
+import dynamic from "next/dynamic";
 import * as React from "react";
+
+const CustomEmailEditor = dynamic(
+  () => import("@/components/CustomEmailEditor"),
+  {
+    loading: () => <p>Loading...</p>,
+    ssr: false,
+  }
+);
 
 export default function Page() {
   return <CustomEmailEditor />;
